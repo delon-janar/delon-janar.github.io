c5d41974743473938c2d487970c735215673f683
delon-janar <delonjanar1@gmail.com>
2023-10-22 15:49:21 +0700

    tambahsectionlagi

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,29 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+const form = document.forms['form-web'];
+const successAlert = document.getElementById('success-alert');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    successAlert.classList.add('d-none');
+
+    if (validateForm()) {
+        setTimeout(() => {
+            successAlert.classList.remove('d-none');
+            form.reset();
+        }, 2000); 
+    }
+    console.log('Form submitted')
+});
+
+function validateForm() {
+    const inputField = form.querySelector('#name');
+    if (inputField.value.trim() === '') {
+        displayError('Name must be filled out.');
+        return false;
+    }
+
+    return true;
+}
